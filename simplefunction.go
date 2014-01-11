59c7970213b691c22a0ae46b09645bed78df50fe
Chris Trail <cstrail@gmail.com>
2014-01-11 10:00:50 -0600

    Changed values in paramets from 5 to 4.

--- simplefunction.go
+++ simplefunction.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("Muiltiply 2 * 5 * 6 = %d\n", MultiPly3Nums(2, 5, 6))
+	fmt.Printf("Muiltiply 2 * 4 * 6 = %d\n", MultiPly3Nums(2, 4, 6))
 }
 
 func MultiPly3Nums(a int, b int, c int) int {
