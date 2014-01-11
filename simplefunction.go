7ae0837eb789752fcba6e6d860ad7740875ce4c6
Chris Trail <cstrail@gmail.com>
2014-01-11 09:12:56 -0600

    first commit

---
+++ simplefunction.go
@@ -0,0 +1,14 @@
+// simplefunction
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Muiltiply 2 * 5 * 6 = %d\n", MultiPly3Nums(2, 5, 6))
+}
+
+func MultiPly3Nums(a int, b int, c int) int {
+	return a * b * c
+}
